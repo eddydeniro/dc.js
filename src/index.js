ca2d08967c70a897bb14bdd24804762024c73078
Edi Supriyadi <eddydeniro@users.noreply.github.com>
2022-08-01 08:06:47 +0700

    Update index.js
    Export gauge chart

--- src/index.js
+++ src/index.js
@@ -42,3 +42,4 @@ export * from './charts/select-menu';
 export * from './charts/series-chart';
 export * from './charts/sunburst-chart';
 export * from './charts/text-filter-widget';
+export * from './charts/gauge-chart';
